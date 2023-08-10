4a68c9ccb407fcaf42538a6c93cf9cf7e56fdf93
angelopol <angelopolgrossi18@gmail.com>
2023-08-10 12:58:51 -0400

    Add comments in TableCenter and output files

--- TableCenter.py
+++ TableCenter.py
@@ -3,8 +3,20 @@ from center import center
 
 def TableCenter(number):
 
+    '''
+    Funcion para centrar el resultado obtenido por la funcion "table".
+
+    number = numero ingresado por el usuario.
+    
+    result = resultado obtenido por "table".
+
+    TableResult = variable generado por el for, representa cada linea de la tabla dentro de "result".
+    '''
+
     result = table(number)
 
     for TableResult in result:
+
+        #Se imprime y centran cada linea de la tabla.
 
         print(center(TableResult))
--- output.py
+++ output.py
@@ -2,4 +2,6 @@ from TableCenter import TableCenter
 
 while True:
 
+    #Se abre un ciclo infinito y se llama a la funcion "TableCenter" a la que se envia el string introducido por el usuario.
+
     TableCenter(input("Type a Number "))
